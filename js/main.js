12f6aafab30c5e5a31d57fae9603ff54f07b8fb6
DiceglieA <angelodiceglie10@gmail.com>
2022-10-25 15:57:34 +0200

    create array + cicle

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const arrEmilan = ['andryshevchenko@gmail.com', 'pippomio@libero.it', 'tempestaperfetta@nesta.com', 'smokingbianco@kaka.br']
+
+let email = prompt("Please enter your email");
+console.log(email)
+
+for (let i = 0; i < arrEmilan.length; i++) {
+ 
+} 
+
