1de3e481a47e5e379b088400fcc2eaadba5a24fd
DiceglieA <angelodiceglie10@gmail.com>
2022-10-25 17:07:12 +0200

    dadi

--- js/main.js
+++ js/main.js
@@ -3,7 +3,34 @@ const arrEmilan = ['andryshevchenko@gmail.com', 'pippomio@libero.it', 'tempestap
 let email = prompt("Please enter your email");
 console.log(email)
 
+let emails = false
+
 for (let i = 0; i < arrEmilan.length; i++) {
- 
+
+    if (email == arrEmilan[i]) {
+        emails == true
+        console.log('corretto')
+    } else {
+        console.log('errore')
+    }
+
+    //
+
 } 
 
+
+
+
+// I DADI 
+
+
+let user = Math.round(Math.random() * 6)
+let ia = Math.round(Math.random() * 6)
+console.log(user, ia)
+
+if (user === ia){
+    console.log('PAREGGIO')
+}   else if (user > ia) {
+    console.log('HAI VINTO')
+}   else 
+    console.log('HAI PERSO')
